2c20cf8b4364ca5bfb4fe43418b5b2b1c6ad84c6
Local Pizza Employee <LocalPizzaEmployee@users.noreply.github.com>
2023-04-26 15:03:58 +0800

    KeyScript comments added

--- Assets/Scripts/KeyScript.cs
+++ Assets/Scripts/KeyScript.cs
@@ -7,6 +7,9 @@ public class KeyScript : Interactable
 
     [SerializeField] GameObject DoorUnlocks;
     [SerializeField] private GameObject UIpanel;
+    //bool isKey1
+    //bool isKey2
+    // GameObject DoorCube;
 
     public override void OnFocus()
     {
@@ -16,6 +19,27 @@ public class KeyScript : Interactable
     public override void OnInteract()
     {
         DoorUnlocks.GetComponent<BasicDoor>().canBeInteracted = true;
+
+        //if(isKey1)
+        //DoorCube.GetComponent<KeyHoleScript>().key1provided = true;
+        //elseif(isKey2)
+        //DoorCube.GetComponent<KeyHoleScript>().key2provided = true;      
+
+        //if(key1provided && key2provided)
+        //anim.setbool("isOpened",true)
+
+        //if(key1provided)
+        //change color  of light
+        //key1complete = true;
+        //if(key2provided)
+        //change color  of light
+        //key2complete = true;
+
+        //update completedKey()
+        //if(key1complete & key2 complete)
+        //anim.setbool("isOpened",true)
+
+
         UIpanel.SetActive(true);
         gameObject.SetActive(false);
     }
